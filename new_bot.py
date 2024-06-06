fa11ccc27558d35fbef8d9b4b33dc65d9d58b7a2
ronaldocestrela <ronaldo@grupoall.in>
2024-06-06 17:38:16 -0300

    correção

--- new_bot.py
+++ new_bot.py
@@ -1,20 +1,89 @@
+import os
+from dotenv import load_dotenv
 import telebot
-from verificar_mensagem import verificar_mensagem
+from telebot import types
+from verificar_mensagem import verificar_mensagem, obter_resposta_botao
 
-# Defina o token de acesso do seu bot
-token = "6866021355:AAF9AzKPMn6qFt188VhsyGjE7eQlE2FPmdo"
+# Carregar as variáveis de ambiente do arquivo .env
+load_dotenv()
 
-# Crie um objeto Bot
-bot = telebot.TeleBot(token)
+# Obter o token do bot das variáveis de ambiente
+token = os.getenv("TELEGRAM_BOT_TOKEN")
 
-# Crie uma função para responder às mensagens
+# Criar um objeto Bot
+try:
+    bot = telebot.TeleBot(token)
+except Exception as e:
+    # Salvar o erro em um arquivo de log
+    with open("log.txt", "a") as arquivo:
+        arquivo.write(str(e))
+    print(f"Ocorreu um erro ao criar o bot: {e}")
+
+# Função para enviar os botões de interatividade
+def enviar_botoes(chat_id, botoes):
+    markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
+    for linha in botoes:
+        botoes_formatados = [types.KeyboardButton(botao) for botao in linha]
+        markup.add(*botoes_formatados)
+    bot.send_message(chat_id, ".", reply_markup=markup)
+
+# Criar uma função para responder às mensagens
 @bot.message_handler()
 def responder(mensagem):
-    resposta = verificar_mensagem(mensagem.text)
-    bot.reply_to(mensagem, resposta)
+    try:
+        codigo, resposta, botoes = verificar_mensagem(mensagem.text)
+        
+        # Se houver um código, perguntar o destino
+        if codigo:
+            enviar_botoes(mensagem.chat.id, botoes)
+            # Salvar o código como uma variável de chat
+            bot.send_message(mensagem.chat.id, resposta)
+            bot.register_next_step_handler(mensagem, obter_destino, codigo)
+        else:
+            # Se não houver código, enviar a resposta
+            bot.send_message(mensagem.chat.id, resposta)
+        
+        print(f"Respondi para {mensagem.from_user.username}: {resposta}")
+    except Exception as e:
+        # Salvar o erro em um arquivo de log
+        with open("log.txt", "a") as arquivo:
+            arquivo.write(str(e))
+        print(f"Ocorreu um erro ao responder a {mensagem.from_user.username}: {e}")
 
-# Adicione um listener para receber mensagens
-# bot.on_message(responder)
+# Função para obter o destino após o usuário escolher
+def obter_destino(mensagem, codigo):
+    try:
+        resposta = obter_resposta_botao(mensagem.text)
+        
+        
+        utm_campaign = ""
+        if mensagem.text == "MONTE SEU BILHETE":
+            utm_campaign = "montesb"
+        elif mensagem.text == "CANAL INSTA":
+            utm_campaign = "canalinsta"
+        elif mensagem.text == "STORYS":
+            utm_campaign = "storys"
+        elif mensagem.text == "FUTEBOL FREE":
+            utm_campaign = "futfree"
+        elif mensagem.text == "FUTEBOL VIP":
+            utm_campaign = "futvip"
+        elif mensagem.text == "ODDS ALTAS":
+            utm_campaign = "oddsaltas"
+        elif mensagem.text == "NBA FREE":
+            utm_campaign = "nbafree"
+        elif mensagem.text == "NBA VIP":
+            utm_campaign = "nbavip"
+        elif mensagem.text == "YOUTUBE":
+            utm_campaign = "yt"
+        
+        link = f"https://go.aff.estrelabetpartners.com/{os.getenv("AFILIATE_CODE")}?campaign_id=13830&utm_campaign={utm_campaign}&aposta={codigo}"
+        bot.send_message(mensagem.chat.id, link)
+        print(f"Usuário {mensagem.from_user.username} escolheu: {mensagem.text}")
+    except Exception as e:
+        # Salvar o erro em um arquivo de log
+        with open("log.txt", "a") as arquivo:
+            arquivo.write(str(e))
+        print(f"Ocorreu um erro ao lidar com a escolha do botão para {mensagem.from_user.username}: {e}")
 
-# Inicie o bot
+# Iniciar o bot
 bot.polling()
--- verificar_mensagem.py
+++ verificar_mensagem.py
@@ -1,7 +1,40 @@
+def gerar_botoes():
+    botoes = [
+        ["MONTE SEU BILHETE", "CANAL INSTA"],
+        ["STORYS", "FUTEBOL FREE"],
+        ["FUTEBOL VIP", "ODDS ALTAS"],
+        ["NBA FREE", "NBA VIP"],
+        ["YOUTUBE"]
+    ]
+    return botoes
+
 def verificar_mensagem(mensagem):
-    if mensagem == "Olá":
-        return "Olá, como você está?"
+    codigo = None
+    if "shareCode=" in mensagem:
+        codigo = mensagem.split("shareCode=")[1]
+        mensagem = mensagem.split("https://")[0]  # Remover link da mensagem
+
+    if codigo:
+        resposta = "Escolha um destino:"
+        botoes = gerar_botoes()
+        return codigo,resposta, botoes
     elif mensagem == "Tchau":
-        return "Tchau, até mais!"
+        resposta = "Tchau, até mais!"
+        return None, resposta, None
     else:
-        return "Não entendi o que você disse"
+        resposta = "Por favor, compartilhe o link com o código da aposta."
+        return None, resposta, None
+
+def obter_resposta_botao(botao):
+    respostas = {
+        "MONTE SEU BILHETE": "Você escolheu MONTE SEU BILHETE.",
+        "CANAL INSTA": "Você escolheu CANAL INSTA.",
+        "STORYS": "Você escolheu STORYS.",
+        "FUTEBOL FREE": "Você escolheu FUTEBOL FREE.",
+        "FUTEBOL VIP": "Você escolheu FUTEBOL VIP.",
+        "ODDS ALTAS": "Você escolheu ODDS ALTAS.",
+        "NBA FREE": "Você escolheu NBA FREE.",
+        "NBA VIP": "Você escolheu NBA VIP.",
+        "YOUTUBE": "Você escolheu YOUTUBE."
+    }
+    return respostas.get(botao, "Opção inválida.")
