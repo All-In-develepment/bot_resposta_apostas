9b88e9a3b499f300ff3b27e616bcd37afc09c45f
Ronaldo Estrela <ronaldoestrela@yahoo.com.br>
2025-03-08 13:00:46 -0300

    feat: Create buttons for Ramon

--- interfaces/telegram_bot.py
+++ interfaces/telegram_bot.py
@@ -1,10 +1,11 @@
 from telegram import Update
-from telegram.ext import Application, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, MessageHandler, filters, ContextTypes, CallbackQueryHandler
 from usecases.echo_message import EchoMessageUseCase
 from usecases.extract_domain import ExtractDomainUseCase
 from usecases.identify_betting import IdentifyBetting
 from usecases.bet_link import BetLink
 from domain.entities import Message
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 class TelegramBot:
     def __init__(self, token: str):
@@ -18,17 +19,31 @@ class TelegramBot:
     def start(self):
         # Adicionamos o handler para processar mensagens
         self.application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, self.handle_message))
+        
+        self.application.add_handler(CallbackQueryHandler(self.handle_button_click))
 
         # Inicia o bot
         self.application.run_polling()
+    
+    def create_buttons(self, options: list):
+        buttons = [[InlineKeyboardButton(option, callback_data=option)] for option in options]
+        return InlineKeyboardMarkup(buttons)
 
     async def handle_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         user_message = update.message.text.strip()
         domain = self.identify_betting.IdentifyBetting(user_message)
         if domain == "EstrelaBet":
             # Gera aposta da EstrelaBet
-            msg_replay = self.bet_link.EstrelaBetLink(user_message)
-            await update.message.reply_text(f"{msg_replay}")
+            # Gera botões para escolher a plataforma
+            options = ["INSTAGRAM / STORY", "INSTAGRAM / REELS", "TELEGRAM", "WHATSAPP", "TRAFEGO PAGO", "DISPARO", "YOUTUBE"]
+            reply_markup = self.create_buttons(options)
+            await update.message.reply_text("Choose an option:", reply_markup=reply_markup)
+            
+            # Salva a mensagem original no contexto do usuário
+            context.user_data["last_message"] = user_message
+            
+            # msg_replay = self.bet_link.EstrelaBetLink(user_message)
+            # await update.message.reply_text(f"{msg_replay}")
         
         if domain == "McGames":
             # Gera aposta da McGames
@@ -40,4 +55,20 @@ class TelegramBot:
             msg_replay = self.bet_link.SportingBetLink(user_message)
             await update.message.reply_text(f"{msg_replay}")
 
-        await update.message.reply_text(f"{domain}")
+        # await update.message.reply_text(f"{domain}")
+
+    async def handle_button_click(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        query = update.callback_query
+        await query.answer()  # Responde a interação para evitar o erro "Botão carregando..."
+
+        selected_option = query.data  # Obtém a opção escolhida
+        user_message = context.user_data.get("last_message", "")
+
+        if user_message:
+            # Gera o link com base na opção e na mensagem original
+            msg_replay = self.bet_link.EstrelaBetLink(user_message, selected_option)
+            response_text = f"Você escolheu: {selected_option}\nAqui está seu link: {msg_replay}"
+        else:
+            response_text = f"Você escolheu: {selected_option}"
+
+        await query.message.reply_text(response_text)
--- usecases/bet_link.py
+++ usecases/bet_link.py
@@ -6,13 +6,27 @@ import os
 load_dotenv()
 
 class BetLink:
-    def EstrelaBetLink(self, url: str) -> str:
+    def EstrelaBetLink(self, url: str, selected_option) -> str:
         afiliate_code = os.getenv("AFILIATE_CODE_ESTRELA_BET")
 
         codigo = url.split("shareCode=")[1]
 
-        link = f"https://go.aff.estrelabetpartners.com/{afiliate_code}?&aposta={codigo}"
-
+        # link = f"https://go.aff.estrelabetpartners.com/{afiliate_code}?&aposta={codigo}"
+        link = ""
+        if selected_option == "INSTAGRAM / STORY":
+            link = f"https://go.aff.estrelabetpartners.com/7hfp3csf?utm_term=instagram&utm_term=story?&aposta={codigo}"
+        elif selected_option == "INSTAGRAM / REELS":
+            link = f"https://go.aff.estrelabetpartners.com/7hfp3csf?utm_term=instagram&utm_term=reels?&aposta={codigo}"
+        elif selected_option == "TELEGRAM":
+            link = f"https://go.aff.estrelabetpartners.com/7hfp3csf?utm_term=telegram?&aposta={codigo}"
+        elif selected_option == "WHATSAPP":
+            link = f"https://go.aff.estrelabetpartners.com/7hfp3csf?utm_term=whatsapp?&aposta={codigo}"
+        elif selected_option == "TRAFEGO PAGO":
+            link = f"https://go.aff.estrelabetpartners.com/7hfp3csf?utm_term=tp?&aposta={codigo}"
+        elif selected_option == "DISPARO":
+            link = f"https://go.aff.estrelabetpartners.com/7hfp3csf?utm_term=disparo?&aposta={codigo}"
+        elif selected_option == "YOUTUBE":
+            link = f"https://go.aff.estrelabetpartners.com/7hfp3csf?utm_term=yt?&aposta={codigo}"
         return link
     
     def McGamesLink(self, url: str) -> str:
