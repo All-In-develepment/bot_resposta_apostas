9703512737435a315a15356907d8f75f1babd26f
Ronaldo Estrela <ronaldoestrela@yahoo.com.br>
2025-04-04 13:44:25 -0300

    fix: update McGamesLink method to include home parameter in generated link

--- usecases/bet_link.py
+++ usecases/bet_link.py
@@ -33,13 +33,9 @@ class BetLink:
     def McGamesLink(self, url: str) -> str:
         afiliate_code = os.getenv("AFILIATE_CODE_MCGAMES")
         
-        base_url = "https://mcgames.bet.br/sports?bscode="
         codigo_aposta = url.split("shareCode=")[1] # Extrair apenas o código
-        # https://mcgames.bet.br/sports?bscode=CODIGOBILHETE&utm_source=IDAFILIADO
-        # link = f"{base_url}{codigo_aposta}&utm_source={afiliate_code}"
-        link = f"https://go.aff.mcgames.bet/{afiliate_code}?shareCode={codigo_aposta}"
+        link = f"https://go.aff.mcgames.bet/{afiliate_code}?shareCode={codigo_aposta}&home=1"
         return link
-        # link = f"{base_url}{afiliate_code}?shareCode={codigo_aposta}"
     
     def SportingBetLink(self, url: str) -> str:
         afiliate_code = os.getenv("AFILIATE_CODE_SPORTING_BET")
