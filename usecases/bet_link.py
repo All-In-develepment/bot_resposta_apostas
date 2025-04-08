6eacfe271302571c245014327970592c78fd9c95
Ronaldo Estrela <ronaldoestrela@yahoo.com.br>
2025-04-08 18:37:33 -0300

    fix: update EsportivaBetLink method to correct URL format

--- usecases/bet_link.py
+++ usecases/bet_link.py
@@ -64,6 +64,5 @@ class BetLink:
         codigo_aposta = url.split("shareCode=")[1]
         
         # https://go.aff.esportivabet.com/3qj2xv8m?shareCode=JNUT8BTXYW
-        link = f"https://go.aff.esportivabet.com/{afiliate_code}?shareCode={codigo_aposta}"
-        
+        link = f"https://go.aff.esportiva.bet/{afiliate_code}?shareCode={codigo_aposta}"
         return link
